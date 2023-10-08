daec90783e15ec3c2cbc5b7945068d5ca61c0fbb
pg2103 <prakhar.gupta.212003@gmail.com>
2023-10-08 17:46:56 +0530

    app updated

--- app.js
+++ app.js
@@ -15,13 +15,14 @@ function addTask(){
     saveData();
 }
 listContainer.addEventListener("click",function(e){
-    if(e.target.tagName==="LI"){
+        if(e.target.tagName==="LI"){
         e.target.classList.toggle("checked");
         saveData();
     }else if(e.target.tagName==="SPAN"){
         e.target.parentElement.remove();
         saveData();
     }
+    
 },false);
 showTask();
 function saveData(){
